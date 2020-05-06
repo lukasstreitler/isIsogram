4f05d57bbb80726bfb1ceac3cf63c142455377cc
lukasstreitler <lukas.streitler@students.fhv.at>
2020-05-06 09:09:55 +0000

    test

--- src/isogram.c
+++ src/isogram.c
@@ -3,5 +3,20 @@
 
 bool is_isogram(const char phrase[]){
 
-    return false;
+    int len = strlen(phrase);
+    for (int i=0; i<len; i++) {
+        char currentchar=phrase[i];
+        int hits=0;
+        for (int j=0; j<len; j++) {
+             if(currentchar==phrase[j] && !(phrase[j] == " "))
+             {
+                 hits++;
+             }
+        }
+        if(hits>1)
+        {
+            return false;
+        }
+    }
+    return true;
 }
--- test/test_isogram.c
+++ test/test_isogram.c
@@ -1,18 +1,45 @@
 #include "vendor/unity.h"
 #include "../src/isogram.h"
+#include "vendor/unity_internals.h"
 #include <stdlib.h>
+#include <string.h>
 
 void test_empty_string(void)
 {
    TEST_ASSERT_TRUE(is_isogram(""));
 }
-
+void test_short_isogram_word(){
+    TEST_ASSERT_TRUE(is_isogram("Auto"));
+}
+void test_butter_isogram(){
+    TEST_ASSERT_FALSE(is_isogram("butter"));
+}
+void test_long_isogram(){
+    TEST_ASSERT_TRUE(is_isogram("Dialogschwerpunkt"));
+}
+void test_no_long_isogram(){
+    TEST_ASSERT_FALSE(is_isogram("sdfasdfasfasfasfasfasdfasdfasdf"));
+}
+void test_aal()
+{
+    TEST_ASSERT_FALSE(is_isogram("Aal"));
+}
+void test_leerzeichen()
+{
+    TEST_ASSERT_FALSE(is_isogram("Des hot an abstand"));
+}
 int main(void)
 {
    UnityBegin("isIsogram");
 
    RUN_TEST(test_empty_string);
-
+   RUN_TEST(test_short_isogram_word);
+   RUN_TEST(test_butter_isogram);
+   RUN_TEST(test_long_isogram);
+   RUN_TEST(test_no_long_isogram);
+   RUN_TEST(test_aal);
+   RUN_TEST(test_leerzeichen);
    UnityEnd();
    return 0;
+
 }
